02d29b5b70b416ff27d225455625925961e17bca
yeilot <yair@amisrael.org>
2026-03-23 20:28:07 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import firebaseConfig from '../firebase-applet-config.json';
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth(app);
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 
 // הגדרות קבועות כדי למנוע בעיות טעינה בפריסה
 const firebaseConfig = {
